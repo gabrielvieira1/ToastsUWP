45610ceebe3cb4ce598cf71cb33c72b18dc844c1
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-02-10 22:01:28 -0300

    How to use toasts

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      ToastContent content = new ToastContent()
+      {
+        Launch = "app-defined-string",
+
+        Visual = new ToastVisual()
+        {
+          BindingGeneric = new ToastBindingGeneric()
+          {
+            Children =
+            {
+              new AdaptiveText()
+              {
+                Text = "Sample"
+              },
+              new AdaptiveText()
+              {
+                Text = "This is a simple toast notification example"
+              }
+            },
+          }
+        },
+
+        //Audio = new ToastAudio()
+        //{
+        //  Src = new Uri("ms-winsoundevent:Notification.Reminder")
+        //}
+
+        Actions = new ToastActionsCustom()
+        {
+          Buttons =
+          {
+            new ToastButton("See Event", "see"),
+            //new ToastButton("See Event", "https://www.bing.com/?cc=br")
+            //{
+            //  ActivationType = ToastActivationType.Protocol
+            //},
+            new ToastButton("Ignore", "cancel"),
+          },
+          Inputs =
+          {
+            new ToastSelectionBox("interest")
+            {
+              DefaultSelectionBoxItemId = "2",
+              Items =
+              {
+                new ToastSelectionBoxItem("1", "Interesting"),
+                new ToastSelectionBoxItem("2", "Not sure"),
+                new ToastSelectionBoxItem("3", "Giving it a miss"),
+              }
+            },
+            new ToastTextBox("comments")
+            {
+              PlaceholderContent = "Type other thoughts"
+            }
+          }
+        }
+      };
+
+      var notification = new ToastNotification(content.GetXml());
+      notification.ExpirationTime = DateTimeOffset.UtcNow.AddMinutes(10);
+      ToastNotificationManager.CreateToastNotifier().Show(notification);
+    }
+  }
+}
